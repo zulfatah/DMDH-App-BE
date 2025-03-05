f2f1370655150ae462125bd713e5b9b88429e1c8
Muhammad zulfatah <zfath423@gmail.com>
2025-03-05 09:35:29 +0700

    pod 2

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const moment = require('moment-hijri');
 const app = express();
 const port = process.env.PORT || 3002;
 
+moment.locale('id');
 // Middleware
 app.use(cors());
 app.use(express.json());
