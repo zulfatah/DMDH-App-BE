ed90951bd8af94b4eb2f4f4e1fbdc5bf01df12f9
Muhammad zulfatah <zfath423@gmail.com>
2025-04-21 14:42:04 +0700

    tambah db port

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const SECRET_KEY = process.env.JWT_SECRET || "rahasia-super-aman";
 // Koneksi ke MySQL menggunakan mysql2/promise
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
